efc4a8d79b2dbd7a89a405cf4dc4262130edd745
alebarber <147847782+alebarber@users.noreply.github.com>
2023-12-09 11:01:53 +0100

    Update EditarUsuariosView.java ADD BUTTON

--- src/main/java/es/uca/iw/views/editarUsuarios/EditarUsuariosView.java
+++ src/main/java/es/uca/iw/views/editarUsuarios/EditarUsuariosView.java
@@ -1,15 +1,15 @@
 package es.uca.iw.views.editarUsuarios;
-
 import jakarta.annotation.security.PermitAll;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.OrderedList;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vaadin.flow.theme.lumo.LumoUtility.AlignItems;
@@ -19,11 +19,12 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import com.vaadin.flow.theme.lumo.LumoUtility.MaxWidth;
 import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 import com.vaadin.flow.theme.lumo.LumoUtility.TextColor;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import es.uca.iw.model.User;
 import es.uca.iw.services.UserService;
 import es.uca.iw.views.MainLayout;
-import es.uca.iw.views.editarUsuarios.UserGalleryViewCard;
+import com.vaadin.flow.component.notification.Notification;
 
 
 @Route(value = "EditarUsuarios", layout = MainLayout.class)
@@ -35,6 +36,7 @@ public class EditarUsuariosView extends VerticalLayout {
     private OrderedList userContainer;
     private ComboBox<String> nameComboBox;
     private Button clearFilterButton;
+    private Button addButtonUser;
 
     public EditarUsuariosView(UserService userService) {
         this.userService = userService;
@@ -47,7 +49,6 @@ public class EditarUsuariosView extends VerticalLayout {
             user.getPassword(), user.getRole(), user.getDni(), user.getEmail()));
         }
     }
-
 
 
 
@@ -78,14 +79,69 @@ public class EditarUsuariosView extends VerticalLayout {
             showAllUsers();
         });
 
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+        addButtonUser = new Button("Añadir Nuevo Usuario", event -> {
+            Dialog dialog = new Dialog();
+
+            TextField newNameField = new TextField("Nombre");
+            TextField newSurnameField = new TextField("Apellidos");
+            TextField newUsernameField = new TextField("Nombre de Usuario");
+            TextField newPasswordField = new TextField("Contraseña");
+            TextField newRoleField = new TextField("Rol");
+            TextField newDniField = new TextField("DNI");
+            TextField newEmailField = new TextField("Email");
+
+            Button saveButton = new Button("Guardar", saveEvent -> {
+                String newName = newNameField.getValue();
+                String newSurname = newSurnameField.getValue();
+                String newUsername = newUsernameField.getValue();
+                String newPassword = newPasswordField.getValue();
+                String newRoleString = newRoleField.getValue();
+                String newDni = newDniField.getValue();
+                String newEmail = newEmailField.getValue();
+
+                User user = new User();
+                user.setName(newName);
+                user.setSurname(newSurname);
+                user.setUsername(newUsername);
+                user.setPassword(newPassword);
+                
+                try {
+                    user.setRole(User.Role.valueOf(newRoleString.toUpperCase()));
+                } catch (IllegalArgumentException e) {
+                    Notification.show("Rol inválido. Use ADMIN o USER.");
+                    return;
+                }
+
+                user.setDni(newDni);
+                user.setEmail(newEmail);
+
+                //Guardar user
+                userService.saveUser(user);
+                Notification.show("Usuario añadido con éxito.");
+                dialog.close();
+                UI.getCurrent().getPage().reload();
+            });
+
+            Button cancelButton = new Button("Cancelar", cancelEvent -> dialog.close());
+
+            dialog.add(newNameField, newSurnameField, newUsernameField, newPasswordField, newRoleField, newDniField, newEmailField,
+                    saveButton, cancelButton);
+
+            dialog.open();
+        });
+    ///////////////////////////////////////////////////////////////////////////////
+
         // Configuración de estilos
         nameComboBox.getStyle().set("align-self", "center");
         nameComboBox.getStyle().set("margin-bottom", "2em");
-
         clearFilterButton.getStyle().set("align-self", "center");
+        addButtonUser.getStyle().set("margin-top", "2.25em");
 
         add(headerContainer);
-        add(new HorizontalLayout(nameComboBox, clearFilterButton));
+        add(new HorizontalLayout(nameComboBox, clearFilterButton, addButtonUser));
 
         userContainer = new OrderedList();
         userContainer.addClassNames(LumoUtility.Gap.MEDIUM, LumoUtility.Display.GRID, LumoUtility.ListStyleType.NONE,
@@ -115,6 +171,8 @@ public class EditarUsuariosView extends VerticalLayout {
         updateUsersContainer(filteredUsers);
     }
 
+
+
     private void updateUsersContainer(List<User> filteredUsers) {
         userContainer.removeAll();
 
@@ -130,7 +188,7 @@ public class EditarUsuariosView extends VerticalLayout {
             }
         }
     }
-/////////////////////////////////////////////////////////////////7
+
     public void showAllUsers() {
         userContainer.removeAll();
         for (User user : users) {
