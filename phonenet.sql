c759c35da20a7c18a90a67ac3003bf08519339fb
AndresMontoro <andres.montorovenegas@alum.uca.es>
2023-12-16 14:08:30 +0100

    Actualizo la BD

--- phonenet.sql
+++ phonenet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-11-2023 a las 20:10:43
+-- Tiempo de generación: 16-12-2023 a las 14:07:08
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -26,11 +26,26 @@ USE `phonenet`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `contract`
+--
+
+CREATE TABLE `contract` (
+  `id` bigint(20) NOT NULL,
+  `end_date` datetime(6) NOT NULL,
+  `start_date` datetime(6) NOT NULL,
+  `product_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `product__id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `product`
 --
 
 CREATE TABLE `product` (
-  `_id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL,
   `available` bit(1) NOT NULL,
   `description` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
@@ -42,26 +57,9 @@ CREATE TABLE `product` (
 -- Volcado de datos para la tabla `product`
 --
 
-INSERT INTO `product` (`_id`, `available`, `description`, `image`, `name`, `price`) VALUES
-(1, b'1', 'Tarifa de verano para aprovechar el sol.', 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw', 'Tarifa de veano', 21.50),
-(102, b'1', 'Tarifa todo en uno para estudiantes. Garantiza una experiencia de navegación de 600Mb simétricos, llamadas ilimitadas en tu fijo y móvil y 20GB de datos móviles.', 'https://www.elmira.es/asset/thumbnail,992,558,center,center/media/elmira/images/2023/04/28/2023042809092957303.jpg', 'Tarifa de vuelta al cole', 30.99);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `product_seq`
---
-
-CREATE TABLE `product_seq` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `product_seq`
---
-
-INSERT INTO `product_seq` (`next_val`) VALUES
-(201);
+INSERT INTO `product` (`id`, `available`, `description`, `image`, `name`, `price`) VALUES
+(1, b'1', 'Tarifa especial para programadores', 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw', 'Hola mundo', 30.45),
+(3, b'1', 'Tarifa de verano para el uso de la piscina', 'https://fotografias.antena3.com/clipping/cmsimages01/2023/05/31/F2862410-C9ED-4F82-B784-E07D81FEA58D/playa-verano_97.jpg?crop=910,512,x114,y0&width=1600&height=900&optimize=low&format=webply', 'Tarifa de verano', 50.00);
 
 -- --------------------------------------------------------
 
@@ -80,7 +78,9 @@ CREATE TABLE `product_type` (
 
 INSERT INTO `product_type` (`product_id`, `type`) VALUES
 (1, 'FIBRA'),
-(102, 'FIBRA');
+(1, 'MOVIL'),
+(3, 'FIBRA'),
+(3, 'MOVIL');
 
 -- --------------------------------------------------------
 
@@ -94,37 +94,74 @@ CREATE TABLE `user` (
   `email` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `role` enum('ADMIN','USER') DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
   `surname` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `dni`, `email`, `name`, `password`, `phone_number`, `surname`, `username`) VALUES
+(2, '32094459Y', 'andresmontoro@gmail.com', 'Andrés', '$2a$10$DP2DCySRiFaTZEdAiEe4IOrpAiSEpUU0.ACutg.8jdsjS0480qsRm', NULL, 'Montoro Venegas', 'andres'),
+(3, '32094459Y', 'AdminPhoneNet@gmail.com', 'Admin', '$2a$10$uP2koRZqfw/gMp2V2w7l8u.5eNrSAJTZvF9JfndCoes4siXCZFppS', NULL, 'Admin', 'admin');
+
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user_seq`
+-- Estructura de tabla para la tabla `user_role`
 --
 
-CREATE TABLE `user_seq` (
-  `next_val` bigint(20) DEFAULT NULL
+CREATE TABLE `user_role` (
+  `id` bigint(20) NOT NULL,
+  `role` enum('ADMIN','USER') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `user_seq`
+-- Volcado de datos para la tabla `user_role`
 --
 
-INSERT INTO `user_seq` (`next_val`) VALUES
-(1);
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'USER'),
+(2, 'ADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `user_id` bigint(20) NOT NULL,
+  `roles_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `user_roles`
+--
+
+INSERT INTO `user_roles` (`user_id`, `roles_id`) VALUES
+(2, 1),
+(3, 2);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `contract`
+--
+ALTER TABLE `contract`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK6gt1bchn8d85oaq1srbhk31n4` (`product_id`),
+  ADD KEY `FK5s7r1nq49s36ndq7nlri6hxf7` (`user_id`);
+
+--
 -- Indices de la tabla `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`_id`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `UK_q2n3melweyrl5d4rqkg7pq6ra` (`description`),
   ADD UNIQUE KEY `UK_k522ciwe39etspfr4ncb790m2` (`image`),
   ADD UNIQUE KEY `UK_jmivyxk9rmgysrmsqw15lqr5b` (`name`);
@@ -144,14 +181,69 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`);
 
 --
+-- Indices de la tabla `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`user_id`,`roles_id`),
+  ADD KEY `FK5i6gd32hnpr2nyf5edlvl9nhw` (`roles_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contract`
+--
+ALTER TABLE `contract`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `contract`
+--
+ALTER TABLE `contract`
+  ADD CONSTRAINT `FK5s7r1nq49s36ndq7nlri6hxf7` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK6gt1bchn8d85oaq1srbhk31n4` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
 --
 -- Filtros para la tabla `product_type`
 --
 ALTER TABLE `product_type`
-  ADD CONSTRAINT `FKo6cvd7sivyf4ji66lnpfnc5pt` FOREIGN KEY (`product_id`) REFERENCES `product` (`_id`);
+  ADD CONSTRAINT `FKo6cvd7sivyf4ji66lnpfnc5pt` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+--
+-- Filtros para la tabla `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `FK55itppkw3i07do3h7qoclqd4k` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK5i6gd32hnpr2nyf5edlvl9nhw` FOREIGN KEY (`roles_id`) REFERENCES `user_role` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
