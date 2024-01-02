6bfa4c58a47b2cb5de4270764d7305f2b58d94e8
Alejandro Barber Delgado <147847782+alebarber@users.noreply.github.com>
2024-01-02 18:22:28 +0100

    Update MiPerfilView.java

--- src/main/java/es/uca/iw/views/miPerfil/MiPerfilView.java
+++ src/main/java/es/uca/iw/views/miPerfil/MiPerfilView.java
@@ -22,10 +22,10 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 
 import es.uca.iw.model.User;
 import es.uca.iw.services.UserDetailsServiceImpl;
-import es.uca.iw.views.MainUserLayout;
+import es.uca.iw.views.MainLayout;
 import jakarta.annotation.security.PermitAll;
 
-@Route(value = "", layout = MainUserLayout.class)
+@Route(value = "", layout = MainLayout.class)
 @PermitAll
 public class MiPerfilView extends VerticalLayout {
     private UserDetailsServiceImpl userService;
