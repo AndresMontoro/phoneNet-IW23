a2e7e4b61b8bd75ea3ea4f3982ea0d060c3e5152
Alejandro Barber Delgado <147847782+alebarber@users.noreply.github.com>
2024-01-09 11:02:30 +0100

    campo routerspeed + restricciones vistas

--- src/main/java/es/uca/iw/views/editarProductos/ImageGalleryViewCard2.java
+++ src/main/java/es/uca/iw/views/editarProductos/ImageGalleryViewCard2.java
@@ -1,6 +1,7 @@
 package es.uca.iw.views.editarProductos;
 import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.Set;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
@@ -29,11 +30,13 @@ import es.uca.iw.model.Product;
 import es.uca.iw.services.ProductService;
 
 
+
 public class ImageGalleryViewCard2 extends ListItem {
     private ProductService productService;
     private long productId;
 
-    public ImageGalleryViewCard2(ProductService productService, long productId, String productName, String productUrl, String productDescription, BigDecimal productPrice, boolean hireVisible) {
+    public ImageGalleryViewCard2(ProductService productService, long productId, String productName, String productUrl, String productDescription,
+     BigDecimal productPrice, int callLimit, int dataUsageLimit, BigDecimal callPrice, BigDecimal dataPrice, int routerSpeed, boolean hireVisible) {
 
         this.productService = productService;
         this.productId = productId;
@@ -53,19 +56,43 @@ public class ImageGalleryViewCard2 extends ListItem {
         image.setAlt(productDescription);
 
         div.add(image);
+        add(div);
 
         Span header = new Span();
         header.addClassNames(FontSize.XLARGE, FontWeight.SEMIBOLD);
         header.setText(productName);
+        add(header);
 
         Span subtitle = new Span();
         subtitle.addClassNames(FontSize.SMALL, TextColor.SECONDARY);
         subtitle.setText(productPrice.toString() + "€/mes");
+        add(subtitle);
+
+        Set<Product.ProductType> productTypes = this.productService.getProductTypes(productId);
+
+        if (productTypes.contains(Product.ProductType.MOVIL)) {
+            Span dataUsage = new Span("Datos: " + dataUsageLimit  / 1024 + "GB" + " - " + dataPrice.toString() + "€/MB");
+            dataUsage.addClassNames(FontSize.SMALL, TextColor.SECONDARY);
+            add(dataUsage);
+        }
+        
+        if (productTypes.contains(Product.ProductType.MOVIL) || productTypes.contains(Product.ProductType.FIJO)) {
+            Span callUsage = new Span("Llamadas: " + callLimit + "min" + " - " + callPrice.toString() + "€/min");
+            callUsage.addClassNames(FontSize.SMALL, TextColor.SECONDARY);
+            add(callUsage);
+        }
+        
+        if (productTypes.contains(Product.ProductType.FIBRA)) {
+            Span rSpeed = new Span("Velocidad del router: " + routerSpeed + "Mbps");
+            rSpeed.addClassNames(FontSize.SMALL, TextColor.SECONDARY);
+            add(rSpeed);
+        }
+
+
 
         Paragraph description = new Paragraph(productDescription);
         description.addClassName(Margin.Vertical.MEDIUM);
-
-        add(div, header, subtitle, description);
+        add(description);
 
 //////////////////////////////////////////////////////////////
 
@@ -89,6 +116,7 @@ public class ImageGalleryViewCard2 extends ListItem {
                 TextField newDataLimit = new TextField("Nuevo Límite de datos");
                 TextField newCallPenalty = new TextField("Nueva Penalización por llamada");
                 TextField newDataPenalty = new TextField("Nueva Penalización por datos");
+                TextField newRouterSpeed = new TextField("Nueva Velocidad del router");
                 Checkbox newAvailable = new Checkbox("Disponible");
         
                 newNameField.setValue(product.getName());
@@ -99,6 +127,7 @@ public class ImageGalleryViewCard2 extends ListItem {
                 newDataLimit.setValue(String.valueOf(product.getDataUsageLimit()));
                 newCallPenalty.setValue(product.getCallPenaltyPrice().toString());
                 newDataPenalty.setValue(product.getDataPenaltyPrice().toString());
+                newRouterSpeed.setValue(String.valueOf(product.getRouterSpeed()));
                 newAvailable.setValue(product.getAvailable());
         
                 Button saveButton = new Button("Guardar", saveEvent -> {
@@ -110,13 +139,14 @@ public class ImageGalleryViewCard2 extends ListItem {
                     int newProductDataLimit = Integer.parseInt(newDataLimit.getValue());
                     BigDecimal newProductCallPenalty = new BigDecimal(newCallPenalty.getValue());
                     BigDecimal newProductDataPenalty = new BigDecimal(newDataPenalty.getValue());
+                    int newProductRouterSpeed = Integer.parseInt(newRouterSpeed.getValue());
                     boolean newProductAvailable = newAvailable.getValue();
         
                     // Actualizar cambios
                     this.productService.editProductWithDetails(productId, newProductName, newProductDescription,
                             newProductImageUrl, newProductPrice,
                             newProductCallLimit, newProductDataLimit, newProductCallPenalty,
-                            newProductDataPenalty, newProductAvailable);
+                            newProductDataPenalty, newProductRouterSpeed, newProductAvailable);
                     dialog.close();
         
                     // Recargar la página
@@ -128,7 +158,7 @@ public class ImageGalleryViewCard2 extends ListItem {
                 // Agregar componentes
                 dialog.add(newNameField, newDescriptionField, newImageUrlField, newPriceField,
                         newCallLimit, newDataLimit, newCallPenalty, newDataPenalty,
-                        newAvailable, saveButton, cancelButton);
+                        newRouterSpeed, newAvailable, saveButton, cancelButton);
                 dialog.open();
             } else {
                 Notification.show("Producto no encontrado");
