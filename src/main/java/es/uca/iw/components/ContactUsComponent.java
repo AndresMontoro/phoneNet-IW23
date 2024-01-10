8bbee7443198b25b8171afbae85519dc9c5e6b2f
EzequielLemos <ezequiel.lemoscardenas@alum.uca.es>
2024-01-10 12:07:31 +0100

    Update ContactUsComponent.java
    Elimino Facebook que no pueden crearse cuentas con nombre de empresa

--- src/main/java/es/uca/iw/components/ContactUsComponent.java
+++ src/main/java/es/uca/iw/components/ContactUsComponent.java
@@ -11,27 +11,23 @@ import com.vaadin.flow.component.html.Span;
 public class ContactUsComponent extends Div {
 
     public ContactUsComponent() {
-        // Mensaje "Contáctanos"
+
         Span contactUsText = new Span("Contáctanos");
         contactUsText.getStyle().set("font-weight", "bold").set("font-size", "1.5rem");
 
-        // Enlaces a las redes sociales con iconos de Vaadin y FontAwesome
-        Anchor facebookLink = createSocialMediaLink("https://www.facebook.com/tuempresa", VaadinIcon.FACEBOOK.create());
         Anchor twitterLink = createSocialMediaLink("https://twitter.com/PhoneNet134982", VaadinIcon.TWITTER.create());
         Anchor gmailLink = createSocialMediaLink("mailto:PhoneNetIW@gmail.com", VaadinIcon.ENVELOPE.create());
 
-        // Crea una fila horizontal para los elementos
-        HorizontalLayout contactLayout = new HorizontalLayout(contactUsText, facebookLink, twitterLink, gmailLink);
+        HorizontalLayout contactLayout = new HorizontalLayout(contactUsText, twitterLink, gmailLink);
         contactLayout.setSpacing(true);
         contactLayout.setAlignItems(FlexComponent.Alignment.CENTER);
 
-        // Añade la fila de elementos al componente principal
         add(contactLayout);
     }
 
     private Anchor createSocialMediaLink(String url, Icon icon) {
         Anchor anchor = new Anchor(url, icon);
-        anchor.setTarget("_blank"); // Abre el enlace en una nueva pestaña
+        anchor.setTarget("_blank"); 
         return anchor;
     }
 }
