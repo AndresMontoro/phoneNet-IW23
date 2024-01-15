d66f7a2ace669afa6334c18aa16ce81dc8f2eca1
Alejandro Barber Delgado <147847782+alebarber@users.noreply.github.com>
2024-01-15 17:14:42 +0100

    editarFacturas nuevos filtros

--- src/main/java/es/uca/iw/views/editarFacturas/EditarFacturasView.java
+++ src/main/java/es/uca/iw/views/editarFacturas/EditarFacturasView.java
@@ -1,6 +1,8 @@
 package es.uca.iw.views.editarFacturas;
 import jakarta.annotation.security.RolesAllowed;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -35,9 +37,10 @@ public class EditarFacturasView extends VerticalLayout {
     private final BillService billService;
     private List<Bill> bills;
     private OrderedList billContainer;
-    private ComboBox<String> nameComboBox;
     private Button clearFilterButton;
     private Button addButton;
+    private ComboBox<String> dniFilterComboBox;
+    private ComboBox<String> fechaFilterComboBox;
 
     
 
@@ -45,7 +48,8 @@ public class EditarFacturasView extends VerticalLayout {
         this.billService = billService;
         bills = billService.getAllBills();
         creartext();
-        loadComboBoxItems();
+        loadDniComboBoxItems();
+        loadFechaComboBoxItems();
 
         for (Bill bill : bills) {
             billContainer.add(new BillGalleryViewCard(billService, bill.getId(), bill.getDate(),bill.getdataConsumed(), bill.getDataTotalPrice(), bill.getminutesConsumed(), bill.getTotalPrice(), bill.getContract().getId()));
@@ -64,13 +68,19 @@ public class EditarFacturasView extends VerticalLayout {
         header.addClassNames(Margin.Bottom.NONE, Margin.Top.XLARGE, FontSize.XXXLARGE);
         headerContainer.add(header);
 
-        nameComboBox = new ComboBox<>("Seleccione una factura");
-        nameComboBox.setAllowCustomValue(true);
-        nameComboBox.addValueChangeListener(event -> filterBillsByContractId(event.getValue()));
+        dniFilterComboBox = new ComboBox<>("Filtrar por DNI");
+        dniFilterComboBox.setAllowCustomValue(true);
+        dniFilterComboBox.addValueChangeListener(event -> filterBillsByDNI(event.getValue()));
+
+        fechaFilterComboBox = new ComboBox<>("Filtrar por Fecha (yyyy-mm-DD)");
+        fechaFilterComboBox.setAllowCustomValue(true);
+        fechaFilterComboBox.addValueChangeListener(event -> filterBillsByFecha(event.getValue()));
 
         clearFilterButton = new Button("Limpiar filtro", event -> {
-            nameComboBox.setValue(null);
-            showAllContracts();
+            fechaFilterComboBox.setValue(null);
+            dniFilterComboBox.setValue(null);
+            loadFechaComboBoxItems(); 
+            showAllBills();
         });
     
         addButton = new Button("Añadir Nueva factura", event -> {
@@ -102,13 +112,15 @@ public class EditarFacturasView extends VerticalLayout {
             dialog.open();
         });
     
-        nameComboBox.getStyle().set("align-self", "center");
-        nameComboBox.getStyle().set("margin-bottom", "2em");
+        fechaFilterComboBox.getStyle().set("align-self", "center");
+        fechaFilterComboBox.getStyle().set("margin-bottom", "2em");
+        dniFilterComboBox.getStyle().set("align-self", "center");
+        dniFilterComboBox.getStyle().set("margin-bottom", "2em");
         clearFilterButton.getStyle().set("align-self", "center");
         addButton.getStyle().set("margin-top", "2.25em");
     
         add(headerContainer);
-        add(new HorizontalLayout(nameComboBox, clearFilterButton, addButton));
+        add(new HorizontalLayout(dniFilterComboBox, fechaFilterComboBox, clearFilterButton, addButton));
     
         billContainer = new OrderedList();
         billContainer.addClassNames(Gap.MEDIUM, Display.GRID, ListStyleType.NONE, Margin.NONE, Padding.NONE);
@@ -119,31 +131,91 @@ public class EditarFacturasView extends VerticalLayout {
 
 
 
-    private void loadComboBoxItems() {
-        nameComboBox.setItems(bills.stream()
-                .map(bill -> bill.getContract().getUser().getName())
+
+    private void loadDniComboBoxItems() {
+        dniFilterComboBox.setItems(bills.stream()
+                .map(bill -> bill.getContract().getUser().getDni())
+                .distinct()
+                .collect(Collectors.toList()));
+
+    }
+
+    private void loadFechaComboBoxItems() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        fechaFilterComboBox.setItems(bills.stream()
+                .map(bill -> bill.getDate().format(formatter))
+                .distinct()
                 .collect(Collectors.toList()));
     }
-    
-    private void filterBillsByContractId(String selectedName) {
-        if (selectedName == null || selectedName.isEmpty()) {
-            showAllContracts();
-            return;
-        }
-    
-        List<Bill> filteredBills = bills.stream()
-                .filter(bill -> bill.getContract().getUser().getName().equals(selectedName))
+
+
+    private void loadFechasByDNI(String selectedDNI) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        List<String> fechasAsociadas = bills.stream()
+                .filter(bill -> bill.getContract().getUser().getDni().equals(selectedDNI))
+                .map(bill -> bill.getDate().format(dateFormatter))
                 .collect(Collectors.toList());
     
+        fechaFilterComboBox.setItems(fechasAsociadas);
+    }
+
+
+
+
+  
+    private void filterBillsByDNI(String selectedDNI) {
+        if (selectedDNI == null || selectedDNI.isEmpty()) {
+            filterBillsByFecha(fechaFilterComboBox.getValue());
+            return;
+        }
+
+        List<Bill> filteredBills = bills.stream()
+                .filter(bill -> bill.getContract().getUser().getDni().equals(selectedDNI))
+                .collect(Collectors.toList());
+
+        loadFechasByDNI(selectedDNI);
+        filterBillsByDNIAndFecha(selectedDNI, fechaFilterComboBox.getValue(), filteredBills);
+    }
+
+
+    private void filterBillsByFecha(String selectedFecha) {
+        if (selectedFecha == null || selectedFecha.isEmpty()) {
+            showAllBills();
+            return;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate selectedFechaDate = LocalDate.parse(selectedFecha, formatter);
+        List<Bill> filteredBills = bills.stream()
+                .filter(bill -> bill.getDate().equals(selectedFechaDate))
+                .collect(Collectors.toList());
+
+        filterBillsByDNIAndFecha(dniFilterComboBox.getValue(), selectedFecha, filteredBills);
+    }
+
+
+
+    private void filterBillsByDNIAndFecha(String selectedDNI, String selectedFecha, List<Bill> filteredBills) {
+        if (selectedDNI != null && !selectedDNI.isEmpty()) {
+            filteredBills = filteredBills.stream()
+                    .filter(bill -> bill.getContract().getUser().getDni().equals(selectedDNI))
+                    .collect(Collectors.toList());
+        }
+
         updateBillContainer(filteredBills);
     }
+
+
+    
+
 
 
     private void updateBillContainer(List<Bill> filteredBills) {
         billContainer.removeAll();
     
         if (filteredBills.isEmpty()) {
-            showAllContracts();
+            showAllBills();
         } else {
             for (Bill bill : filteredBills) {
                 billContainer.add(new BillGalleryViewCard(billService, bill.getId(), bill.getDate(),bill.getdataConsumed(), bill.getDataTotalPrice(), bill.getminutesConsumed(), bill.getTotalPrice(), bill.getContract().getId()));
@@ -151,7 +223,7 @@ public class EditarFacturasView extends VerticalLayout {
         }
     }
 
-    public void showAllContracts(){
+    public void showAllBills(){
         billContainer.removeAll();
         for (Bill bill : bills) {
             billContainer.add(new BillGalleryViewCard(billService, bill.getId(), bill.getDate(),bill.getdataConsumed(), bill.getDataTotalPrice(), bill.getminutesConsumed(), bill.getTotalPrice(), bill.getContract().getId()));
