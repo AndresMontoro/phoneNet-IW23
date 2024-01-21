bbd3680626c9a099924cb20c1eac251aaf47de84
AndresMontoro <andres.montorovenegas@alum.uca.es>
2024-01-21 13:18:42 +0100

    Nueva vista GestionarProductos

--- src/main/java/es/uca/iw/views/editarProductos/EditarProductosView.java
+++ src/main/java/es/uca/iw/views/editarProductos/EditarProductosView.java
@@ -2,14 +2,16 @@ package es.uca.iw.views.editarProductos;
 import jakarta.annotation.security.RolesAllowed;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import com.vaadin.flow.component.combobox.ComboBox;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.OrderedList;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -21,6 +23,7 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Display;
 import com.vaadin.flow.theme.lumo.LumoUtility.FlexDirection;
 import com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import java.util.stream.Collectors;
 
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Horizontal;
@@ -31,6 +34,7 @@ import es.uca.iw.services.ProductService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.notification.Notification;
 import es.uca.iw.views.MainAdminLayout;
 
@@ -40,9 +44,8 @@ public class EditarProductosView extends VerticalLayout {
 
     private final ProductService productService;
     private List<Product> products;
-    private ComboBox<String> nameComboBox;
-    private Button clearFilterButton;
-    private Button addButton;
+    private Grid<Product> grid;
+    ComboBox<String> filterComboBox;
 
     public EditarProductosView(ProductService productService) {
         addClassNames(Padding.MEDIUM, Display.FLEX, FlexDirection.COLUMN, Gap.LARGE, Horizontal.AUTO, Vertical.AUTO);
@@ -54,15 +57,34 @@ public class EditarProductosView extends VerticalLayout {
         
         this.productService = productService;
         products = productService.findAll();
-        // creartext();
-        // loadComboBoxItems();
 
-        // for (Product product : products) {
-        //         imageContainer.add(new ImageGalleryViewCard2(productService,product.getId(), product.getName(), product.getImage(),
-        //                 product.getDescription(), product.getPrice(), product.getCallLimit(), product.getDataUsageLimit(), product.getCallPenaltyPrice(), product.getDataPenaltyPrice(), product.getRouterSpeed(),false));
-        // }
+        filterComboBox = new ComboBox<>("Filtrar por Nombre");
+        filterComboBox.setItems(products.stream().map(Product::getName).collect(Collectors.toList()));
+        filterComboBox.setPlaceholder("Seleccione un nombre");
+        filterComboBox.addValueChangeListener(event -> filterGridByName(event.getValue()));
+        filterComboBox.getStyle().set("align-self", "center");
+        filterComboBox.getStyle().set("margin-bottom", "2em");
+        add(filterComboBox);
 
-        Grid<Product> grid = new Grid<>(Product.class, false);
+        Button clearFilterButton = new Button("Limpiar filtro", clearEvent -> {
+            filterComboBox.clear();
+            grid.setItems(products);
+        });
+        clearFilterButton.getStyle().set("align-self", "center");
+        add(clearFilterButton);
+
+        HorizontalLayout filteHorizontalLayout = new HorizontalLayout(filterComboBox, clearFilterButton);
+        filteHorizontalLayout.setSpacing(true);
+        filteHorizontalLayout.setAlignItems(Alignment.CENTER);
+        filteHorizontalLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+        filteHorizontalLayout.setMargin(isAttached());
+        
+        add(filteHorizontalLayout);
+
+        Button addProductButton = new Button("Añadir productos", addEvent -> showAddProductDialog());
+        add(addProductButton);
+
+        grid = new Grid<>(Product.class, false);
         grid.setWidthFull();
         grid.setItems(products);
         grid.addColumn(Product::getName).setHeader("Nombre");
@@ -89,132 +111,75 @@ public class EditarProductosView extends VerticalLayout {
         add(grid);
     }
 
-    // private void creartext() {
-    //     addClassNames("image-gallery-view");
-    //     addClassNames(MaxWidth.SCREEN_LARGE, Margin.Horizontal.AUTO);
+    private void filterGridByName(String selectedName) {
+        if (selectedName != null && !selectedName.isEmpty()) {
+            List<Product> filteredProducts = products.stream()
+                    .filter(product -> product.getName().equalsIgnoreCase(selectedName))
+                    .collect(Collectors.toList());
 
-    //     HorizontalLayout container = new HorizontalLayout();
-    //     container.addClassNames(AlignItems.CENTER, JustifyContent.BETWEEN);
-        
+            grid.setItems(filteredProducts);
+        } else {
+            grid.setItems(products);
+        }
+    }
 
-    //     VerticalLayout headerContainer = new VerticalLayout();
-    //     H2 header = new H2("Lista de productos"); // texto 1
-    //     header.addClassNames(Margin.Bottom.NONE, Margin.Top.NONE, FontSize.XXXLARGE);
-    //     headerContainer.add(header);
+    private void showAddProductDialog() {
+        Dialog dialog = new Dialog();
 
-    //     nameComboBox = new ComboBox<>("Seleccione un Producto");
-    //     nameComboBox.setAllowCustomValue(true);
-    //     nameComboBox.addValueChangeListener(event -> filterProductsByName(event.getValue()));
+        TextField newNameField = new TextField("Nombre");
+        TextField newDescriptionField = new TextField("Descripción");
+        TextField newImageUrlField = new TextField("URL de Imagen");
+        TextField newPriceField = new TextField("Precio");
+        TextField newCallLimit = new TextField("Límite de llamadas");
+        TextField newDataUsageLimit = new TextField("Límite de datos");
+        TextField newCallPenalty = new TextField("Penalización por llamada");
+        TextField newDataPenalty = new TextField("Penalización por datos");
+        TextField newRouterSpeed = new TextField("Velocidad del router");
+        Checkbox availableCheckbox = new Checkbox("Disponible");
+        Checkbox fiberTypeCheckbox = new Checkbox("Fibra");
+        Checkbox mobileTypeCheckbox = new Checkbox("Móvil");
+        Checkbox fixedTypeCheckbox = new Checkbox("Fijo");
 
-    //     clearFilterButton = new Button("Limpiar filtro", event -> {
-    //         nameComboBox.setValue(null);
-    //         showAllProducts();
-    //     });
+        FormLayout form = new FormLayout();
+        form.add(newNameField, newDescriptionField, newImageUrlField, newPriceField,
+            newCallLimit, newDataUsageLimit, newCallPenalty, newDataPenalty,
+            newRouterSpeed, availableCheckbox, fiberTypeCheckbox, mobileTypeCheckbox, fixedTypeCheckbox);
 
-    //     addButton = new Button("Añadir Nuevo Producto", event -> {
-    //         Dialog dialog = new Dialog();
+        Button saveButton = new Button("Guardar", saveEvent -> {
+            String newProductName = newNameField.getValue();
+            String newProductDescription = newDescriptionField.getValue();
+            String newProductImageUrl = newImageUrlField.getValue();
+            BigDecimal newProductPrice = new BigDecimal(newPriceField.getValue());
+            Integer newProductCallLimit = Integer.parseInt(newCallLimit.getValue());
+            Integer newProductDataUsageLimit = Integer.parseInt(newDataUsageLimit.getValue());
+            BigDecimal newProductCallPenalty = new BigDecimal(newCallPenalty.getValue());
+            BigDecimal newProductDataPenalty = new BigDecimal(newDataPenalty.getValue());
+            Integer newProductRouterSpeed = Integer.parseInt(newRouterSpeed.getValue());
+            boolean available = availableCheckbox.getValue();
+    
+            Set<Product.ProductType> productTypes = new HashSet<>();
+            if (fiberTypeCheckbox.getValue()) productTypes.add(Product.ProductType.FIBRA);
+            if (mobileTypeCheckbox.getValue()) productTypes.add(Product.ProductType.MOVIL);
+            if (fixedTypeCheckbox.getValue()) productTypes.add(Product.ProductType.FIJO);
+    
+            productService.saveProductWithDetails(newProductName, newProductDescription, newProductImageUrl,
+                newProductPrice, newProductCallLimit, newProductDataUsageLimit, newProductCallPenalty, newProductDataPenalty, newProductRouterSpeed, available, productTypes);
 
-    //         TextField newNameField = new TextField("Nombre");
-    //         TextField newDescriptionField = new TextField("Descripción");
-    //         TextField newImageUrlField = new TextField("URL de Imagen");
-    //         TextField newPriceField = new TextField("Precio");
-    //         TextField newCallLimit = new TextField("Límite de llamadas");
-    //         TextField newDataUsageLimit = new TextField("Límite de datos");
-    //         TextField newCallPenalty = new TextField("Penalización por llamada");
-    //         TextField newDataPenalty = new TextField("Penalización por datos");
-    //         TextField newRouterSpeed = new TextField("Velocidad del router");
+            Notification.show("Producto añadido con éxito.");
+            dialog.close();
+            UI.getCurrent().getPage().reload();
+        });
 
-    //         Checkbox availableCheckbox = new Checkbox("Disponible");
-    //         Checkbox fiberTypeCheckbox = new Checkbox("Fibra");
-    //         Checkbox mobileTypeCheckbox = new Checkbox("Móvil");
-    //         Checkbox fixedTypeCheckbox = new Checkbox("Fijo");
+        Button cancelButton = new Button("Cancelar", cancelEvent -> dialog.close());
+        HorizontalLayout buttonContainer = new HorizontalLayout();
+        buttonContainer.add(saveButton, cancelButton);
+        buttonContainer.setAlignItems(Alignment.END);
+        buttonContainer.setJustifyContentMode(JustifyContentMode.CENTER);
+        buttonContainer.setMargin(isAttached());
 
-
-    //         Button saveButton = new Button("Guardar", saveEvent -> {
-    //             String newProductName = newNameField.getValue();
-    //             String newProductDescription = newDescriptionField.getValue();
-    //             String newProductImageUrl = newImageUrlField.getValue();
-    //             BigDecimal newProductPrice = new BigDecimal(newPriceField.getValue());
-                
-    //             Integer newProductCallLimit = Integer.parseInt(newCallLimit.getValue());
-    //             Integer newProductDataUsageLimit = Integer.parseInt(newDataUsageLimit.getValue());
-    //             BigDecimal newProductCallPenalty = new BigDecimal(newCallPenalty.getValue());
-    //             BigDecimal newProductDataPenalty = new BigDecimal(newDataPenalty.getValue());
-    //             Integer newProductRouterSpeed = Integer.parseInt(newRouterSpeed.getValue());
-    //             boolean available = availableCheckbox.getValue();
-     
-    //             Set<Product.ProductType> productTypes = new HashSet<>();
-    //             if (fiberTypeCheckbox.getValue()) productTypes.add(Product.ProductType.FIBRA);
-    //             if (mobileTypeCheckbox.getValue()) productTypes.add(Product.ProductType.MOVIL);
-    //             if (fixedTypeCheckbox.getValue()) productTypes.add(Product.ProductType.FIJO);
-        
-    //             productService.saveProductWithDetails(newProductName, newProductDescription, newProductImageUrl,
-    //                     newProductPrice, newProductCallLimit,
-    //                     newProductDataUsageLimit, newProductCallPenalty, newProductDataPenalty, newProductRouterSpeed, available, productTypes);
-
-    //             Notification.show("Producto añadido con éxito.");
-    //             dialog.close();
-    //             UI.getCurrent().getPage().reload();
-    //         });
-
-    //         Button cancelButton = new Button("Cancelar", cancelEvent -> dialog.close());
-
-    //         dialog.add(newNameField, newDescriptionField, newImageUrlField, newPriceField, newCallLimit, newDataUsageLimit, newCallPenalty, newDataPenalty, newRouterSpeed, availableCheckbox, fiberTypeCheckbox, mobileTypeCheckbox, fixedTypeCheckbox, saveButton, cancelButton);
-
-    //         dialog.open();
-    //     });
-
-    //     nameComboBox.getStyle().set("align-self", "center");
-    //     nameComboBox.getStyle().set("margin-bottom", "2em");    
-    //     clearFilterButton.getStyle().set("align-self", "center");
-    //     addButton.getStyle().set("margin-top", "2.25em");
-        
-    //     add(headerContainer);
-    //     add(new HorizontalLayout(nameComboBox, clearFilterButton, addButton));
-
-    //     imageContainer = new OrderedList();
-    //     imageContainer.addClassNames(Gap.MEDIUM, Display.GRID, ListStyleType.NONE, Margin.NONE, Padding.NONE);
-    //     add(imageContainer);
-    // }
-
-    // private void loadComboBoxItems() {
-    //     nameComboBox.setItems(products.stream().map(Product::getName).collect(Collectors.toList()));
-    // }
-
-    // private void filterProductsByName(String selectedName) {
-    //     if (selectedName == null || selectedName.isEmpty()) {
-    //         showAllProducts();
-    //         return;
-    //     }
-
-    //     List<Product> filteredProducts = products.stream()
-    //             .filter(product -> product.getName().equals(selectedName))
-    //             .collect(Collectors.toList());
-
-    //     updateImageContainer(filteredProducts);
-    // }
-
-    // private void updateImageContainer(List<Product> filteredProducts) {
-    //     imageContainer.removeAll();
-
-    //     if (filteredProducts.isEmpty()) {
-    //         showAllProducts();
-    //     } else {
-    //         for (Product product : filteredProducts) {
-    //                 imageContainer.add(new ImageGalleryViewCard2(productService,product.getId(), product.getName(), product.getImage(),
-    //                     product.getDescription(), product.getPrice(), product.getCallLimit(), product.getDataUsageLimit(), product.getCallPenaltyPrice(), product.getDataPenaltyPrice(), product.getRouterSpeed(), false));   
-    //         }
-    //     }
-    // }
-
-    // public void showAllProducts() {
-    //     // Mostrar todos los productos
-    //     imageContainer.removeAll();
-    //     for (Product product : products) {
-    //             imageContainer.add(new ImageGalleryViewCard2(productService,product.getId(), product.getName(), product.getImage(),
-    //                     product.getDescription(), product.getPrice(), product.getCallLimit(), product.getDataUsageLimit(), product.getCallPenaltyPrice(), product.getDataPenaltyPrice(), product.getRouterSpeed(), false));
-    //     }
-    // }
+        dialog.add(form, buttonContainer);
+        dialog.open();
+    }
 
     private Button createEditButton(Long productId) {
         Button editButton = new Button(VaadinIcon.EDIT.create(), editEvent -> {
@@ -222,8 +187,6 @@ public class EditarProductosView extends VerticalLayout {
             if (optionalProduct.isPresent()) {
                 Product product = optionalProduct.get();
                 Dialog dialog = new Dialog();
-
-                FormLayout form = new FormLayout();
         
                 TextField newNameField = new TextField("Nuevo Nombre");
                 TextField newDescriptionField = new TextField("Nueva Descripción");
@@ -247,6 +210,7 @@ public class EditarProductosView extends VerticalLayout {
                 newRouterSpeed.setValue(String.valueOf(product.getRouterSpeed()));
                 newAvailable.setValue(product.getAvailable());
 
+                FormLayout form = new FormLayout();
                 form.add(newNameField, newDescriptionField, newImageUrlField, newPriceField,
                     newCallLimit, newDataLimit, newCallPenalty, newDataPenalty,
                     newRouterSpeed, newAvailable);
@@ -281,7 +245,6 @@ public class EditarProductosView extends VerticalLayout {
                 buttonContainer.setAlignItems(Alignment.CENTER);
                 buttonContainer.setJustifyContentMode(JustifyContentMode.CENTER);
                 buttonContainer.setMargin(isAttached());
-
         
                 // Agregar componentes
                 dialog.add(form, buttonContainer);
